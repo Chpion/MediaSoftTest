f0bf9727b2ffc95e057c7a26c40ece05b5ebf97b
Агафонов Владислав Алексеевич <78472316+Chpion@users.noreply.github.com>
2024-07-01 22:28:16 +0400

    Delete src/main directory

--- src/main/java/com/mediasoft/agafonov/exceptions/ProductNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.mediasoft.agafonov.exceptions;
-
-public class ProductNotFoundException extends RuntimeException {
-    public ProductNotFoundException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/mediasoft/agafonov/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mediasoft.agafonov.repository;
-
-import com.mediasoft.agafonov.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
